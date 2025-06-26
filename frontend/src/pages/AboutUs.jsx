d4fe7e27feee4e0ac0d8b648b9771f78b5805c9c
Lenin Zuna <Lenin.zuna23@gmail.com>
2025-06-26 17:35:36 -0400

    About Us update

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>About Us Page Content</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni qui
+        animi vero mollitia blanditiis doloribus facilis consectetur, quasi
+        delectus nobis optio explicabo suscipit expedita aut. Cupiditate
+        blanditiis quisquam suscipit. Deleniti.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUsPage;
