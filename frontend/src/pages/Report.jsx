7c3748f325d2a382e7aee9bf0856a0e0ad1ce1f6
Henry Tan <htan0705@gmail.com>
2025-06-26 00:53:08 -0400

    Initial Report Form

--- frontend/src/pages/Report.jsx
+++ frontend/src/pages/Report.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import ReportForm from "../components/ReportForm";
 
 const ReportPage = () => {
-  return <div>Report Page</div>;
+  return (
+    <div className="bg-white p-10 mt-10 w-full">
+      <div className="flex flex-col mx-[25%]">
+        <h1 className="text-black text-left mb-5">
+          Report a Lost or Found Pet
+        </h1>
+
+        <h3 className="text-black">
+          Submit your report to receive alerts, and have your report forwarded
+          to local and national databases
+        </h3>
+
+        <ReportForm />
+      </div>
+    </div>
+  );
 };
 
 export default ReportPage;
