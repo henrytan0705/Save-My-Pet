7e10a931d38f73cb33c79456d54028f8d924c867
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2025-06-26 18:32:52 -0400

    updated .js

--- frontend/public/map.js
+++ frontend/public/map.js
@@ -3,3 +3,8 @@ const button = document.getElementById("exampleId");
 button.addEventListener("click", function () {
   alert("Button Pressed!");
 });
+
+function toggleMenu() {
+    const nav = document.getElementById("navbar");
+    nav.classList.toggle("show");
+}
