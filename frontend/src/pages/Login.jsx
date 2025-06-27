9bb8ef066566cc077605ba096bb5cb89104c74ae
Henry Tan <htan0705@gmail.com>
2025-06-26 23:04:54 -0400

    Touch up Login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const LoginPage = () => {
-  return <div>Login Page Content</div>;
+  return (
+    <div className="my-[10%] mx-[10%]">
+      <h1 className="text-center">Login Page</h1>
+
+      <p className="mt-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni qui
+        animi vero mollitia blanditiis doloribus facilis consectetur, quasi
+        delectus nobis optio explicabo suscipit expedita aut. Cupiditate
+        blanditiis quisquam suscipit. Deleniti.
+      </p>
+
+      <p className="mt-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni qui
+        animi vero mollitia blanditiis doloribus facilis consectetur, quasi
+        delectus nobis optio explicabo suscipit expedita aut. Cupiditate
+        blanditiis quisquam suscipit. Deleniti.
+      </p>
+
+      <p className="mt-5">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni qui
+        animi vero mollitia blanditiis doloribus facilis consectetur, quasi
+        delectus nobis optio explicabo suscipit expedita aut. Cupiditate
+        blanditiis quisquam suscipit. Deleniti.
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
