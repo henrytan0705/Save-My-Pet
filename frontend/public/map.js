6e9a2cadb97f1e0e8a847d17334601a172c9d1fb
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2025-06-27 09:16:14 -0400

    Updated html, css, and js files to incorporate a dynamic leaflet.js map

--- frontend/public/map.js
+++ frontend/public/map.js
@@ -1,10 +1,17 @@
 const button = document.getElementById("exampleId");
 
-button.addEventListener("click", function () {
-  alert("Button Pressed!");
-});
-
 function toggleMenu() {
     const nav = document.getElementById("navbar");
     nav.classList.toggle("show");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const map = L.map("map").setView([40.7831, -73.9712], 12);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+       '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  }).addTo(map);      
+});
+
+
+
