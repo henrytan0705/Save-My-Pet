ade9b58c57bd3380a25e20f897f331a52765822e
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2025-06-27 10:13:32 -0400

    added markers on map to show reported pets from mock JSON file

--- frontend/public/map.js
+++ frontend/public/map.js
@@ -10,8 +10,37 @@ document.addEventListener("DOMContentLoaded", function () {
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
        '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-  }).addTo(map);      
+  }).addTo(map);   
+  
+  fetch("mockPets.json")
+    .then((res) =>res.json())
+    .then((data) => {
+        data.pets.forEach((pet) => {
+            let iconColor;
+            if(pet.status === "In Danger") iconColor = "red";
+            else if(pet.status === "Rescued") iconColor = "green";
+            else if(pet.status === "Missing") iconColor = "blue";
+
+            const icon = L.icon({
+                iconUrl: `https://maps.google.com/mapfiles/ms/icons/${iconColor}-dot.png`,
+                iconSize: [32, 32],
+                iconAnchor: [16, 32],
+                popupAnchor: [0, -32],
+            });
+
+            L.marker([pet.lat, pet.lng], { icon: icon})
+                .addTo(map)
+                .bindPopup(
+                    `<strong>${pet.name}</strong><br>Status: ${pet.status}<br>${pet.description}`
+            );
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to load pet data", err);
+    });
 });
 
 
+     
+                
 
