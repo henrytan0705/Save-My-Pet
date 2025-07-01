ac2c9f66337df7fecf2f6dc7b29d79085f499b10
Henry Tan <htan0705@gmail.com>
2025-07-01 19:07:34 -0400

    Initial node server setup

---
+++ server/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("GET API call to User endpoint.");
+});
+
+router.post("/", (req, res) => {
+  res.send("POST API call to User endpoint.");
+});
+
+router.put("/", (req, res) => {
+  res.send("PUT API call to User endpoint.");
+});
+
+router.delete("/", (req, res) => {
+  res.send("DELETE API call to User endpoint.");
+});
+
+module.exports = router;
