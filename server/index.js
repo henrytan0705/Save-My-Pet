9ee8bec8bd1d3eca306184e694db42f58b690081
Henry Tan <htan0705@gmail.com>
2025-07-04 15:45:36 -0400

    Allow dynamic cors for vercel previews

--- server/index.js
+++ server/index.js
@@ -8,8 +8,29 @@ const connectDB = require("./config/db");
 const usersRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 
+const allowedOrigins = [
+  "http://localhost:5173", // Local dev
+  "https://save-my-pet-project.vercel.app/", // Production
+];
+
+const dynamicCors = (req, callback) => {
+  const origin = req.header("Origin");
+
+  if (
+    allowedOrigins.includes(origin) ||
+    /^https:\/\/.*\.vercel\.app$/.test(origin) // Allow all Vercel preview deployments
+  ) {
+    callback(null, {
+      origin: true,
+      credentials: true,
+    });
+  } else {
+    callback(new Error("Not allowed by CORS"));
+  }
+};
+
 app.use(express.json()); // Parse incoming JSON requests
-app.use(cors()); // Enable CORS for cross-origin requests
+app.use(cors(dynamicCors)); // Enable CORS for cross-origin requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 
 // connect to mongoDB
