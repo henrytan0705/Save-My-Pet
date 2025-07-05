eb28236e682c3f1da0ec05e586b0d093df81ae83
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2025-07-05 12:18:01 -0400

    Added leaflet map to MapDisplay

--- frontend/src/components/MapDisplay.jsx
+++ frontend/src/components/MapDisplay.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MapDisplay = () => {
+    const mapRef = useRef(null);
+    const [pets, setPets] = useState([]);
+
+    useEffect (() => {
+        fetch("/mockPets.json")
+            .then((res) => res.json())
+            .then((data) => setPets(data.pets))
+    }, []);
+
+    useEffect (() => { 
+        const map = L.map("leaflet-map").setView([40.7831, -73.9712], 12);
+        mapRef.current = map;
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+            '© <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        }).addTo(map);  
+        
+        pets.forEach((pet) => {
+            const color = pet.status === "In Danger" ? "red" : pet.status === "Rescued" ? "green" : pet.status === "Missing" ? "blue" : "gray";
+            const icon = L.icon({
+                iconUrl: `https://maps.google.com/mapfiles/ms/icons/${color}-dot.png`,
+                iconSize: [32, 32],
+            });
+            L.marker([pet.lat, pet.lng], {icon})
+                .addTo(map)
+                .bindPopup(`<b>${pet.name}</b><br/>${pet.description}`);
+        });
+    }, [pets]);
+
   return (
-    <div>
-        /* Leaflet map here */
+    <div id="leaflet-map" className="w-full h-[400px] rounded-md shadow-md z-0">
     </div>
   );
 };
