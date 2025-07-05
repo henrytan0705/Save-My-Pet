ef06ff5216cadc4915d163042c8b825909e59509
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2025-07-05 11:18:46 -0400

    Added map components to Map.jsx with a flex layout

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import MapDisplay from "../components/map/MapDisplay";
+import PetList from "../components/map/PetList";
+import MapFilters from "../components/map/MapFilters";
+import MapLegend from "../components/map/MapLegend";
 
 const Map = () => {
   return (
-    <div>
-      <h1>Map Content</h1>
-      <p>
-        map intro
-      </p>
-    </div>
+    <main className="pt-36 px-4 min-h-screen">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-6">
+            {/* Pet List */}
+            <section className="flex-1 min-w-[250px]">
+                <PetList />
+            </section>
+
+            {/* Map Display */}
+            <section className="flex-1 min-w-[300px]">
+                <MapDisplay />
+            </section>
+        
+            {/* Filters & Legend */}
+            <aside className="flex-1 min-w-[200px]">
+                <MapFilters />
+                <MapLegend />
+            </aside>
+        </div>
+    </main>
   );
 };
 
