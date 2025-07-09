6963fc63dfcc265a453a5ee19652558b9785fd2d
Lenin Zuna <Lenin.zuna23@gmail.com>
2025-07-08 22:48:40 -0400

    Footer Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import AboutUsPage from "./pages/AboutUs";
 import LostPetsPage from "./pages/LostPets";
@@ -10,37 +9,36 @@ import CommunitiesPage from "./pages/Communities";
 import ReportPage from "./pages/Report";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-// import "./App.css";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
+    return (
+        <BrowserRouter>
+            <div className="min-h-screen flex flex-col">
+                <Navbar />
 
-      <div className="mt-20">
-        <Routes>
-          {/* General Page Routes */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutUsPage />} />
-          <Route path="/lostPets" element={<LostPetsPage />} />
-          <Route path="/foundPets" element={<FoundPetsPage />} />
-          <Route path="/communities" element={<CommunitiesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/report" element={<ReportPage />} />
+                <main className="flex-grow mt-20">
+                    <Routes>
+                        {/* General Page Routes */}
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<AboutUsPage />} />
+                        <Route path="/lostPets" element={<LostPetsPage />} />
+                        <Route path="/foundPets" element={<FoundPetsPage />} />
+                        <Route path="/communities" element={<CommunitiesPage />} />
+                        <Route path="/contact" element={<ContactPage />} />
+                        <Route path="/report" element={<ReportPage />} />
 
-          {/* Routes for user login/sign up*/}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </div>
+                        {/* Routes for user login/sign up*/}
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                    </Routes>
+                </main>
 
-      <Footer />
-    </BrowserRouter>
-  );
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
