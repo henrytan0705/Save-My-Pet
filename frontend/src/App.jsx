db9ced024b644d4b9c59224dad0f1092586e7afd
Lenin Zuna <Lenin.zuna23@gmail.com>
2025-07-09 19:58:18 -0400

    Map link fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/Home";
 import AboutUsPage from "./pages/AboutUs";
+import MapPage from "./pages/Map";
 import LostPetsPage from "./pages/LostPets";
 import FoundPetsPage from "./pages/FoundPets";
 import ContactPage from "./pages/Contact";
@@ -9,7 +10,6 @@ import CommunitiesPage from "./pages/Communities";
 import ReportPage from "./pages/Report";
 import LoginPage from "./pages/Login";
 import RegisterPage from "./pages/Register";
-import MapPage from "./pages/Map";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -24,6 +24,7 @@ function App() {
                         {/* General Page Routes */}
                         <Route path="/" element={<HomePage />} />
                         <Route path="/about" element={<AboutUsPage />} />
+                        <Route path="/map" element={<MapPage />} />
                         <Route path="/lostPets" element={<LostPetsPage />} />
                         <Route path="/foundPets" element={<FoundPetsPage />} />
                         <Route path="/communities" element={<CommunitiesPage />} />
@@ -49,13 +50,12 @@ function App() {
           {/* General Page Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/map" element={<MapPage />} />
           <Route path="/lostPets" element={<LostPetsPage />} />
           <Route path="/foundPets" element={<FoundPetsPage />} />
           <Route path="/communities" element={<CommunitiesPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/report" element={<ReportPage />} />
-          <Route path="/map" element={<MapPage />} />
-
           {/* Routes for user login/sign up*/}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
