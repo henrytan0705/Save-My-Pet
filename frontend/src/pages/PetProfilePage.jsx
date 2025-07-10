3395c69b978bd6861a759783a7106e36ab6d70a8
Lenin Zuna <Lenin.zuna23@gmail.com>
2025-07-10 00:09:53 -0400

    PetProfilePage

---
+++ frontend/src/pages/PetProfilePage.jsx
@@ -0,0 +1,20 @@
+﻿import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import PetProfile from "../components/PetProfile";
+
+const PetProfilePage = () => {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Navbar />
+
+            <div className="flex-grow my-40 mx-[10%]">
+                <PetProfile />
+            </div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default PetProfilePage;
